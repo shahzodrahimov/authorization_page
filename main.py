f84070f66b30202e482e5e60a6957df3055565ef
Shahzod <shahzod.rahimov21@gmail.com>
2022-03-13 04:51:42 +0500

    Asosiy metodlar qo'shildi

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import sys
+from os import system
 
-print("Hello World?")
+
+class Authorization:
+    def __init__(self):      # Name
+        pass
+
+    def initial_page(self):  # Asosiy sahifa -> Name
+        pass
+
+    def register(self):      # Registratsiya qismi -> Name
+        pass
+
+    def log_in(self):        # Log in qismi -> Name
+        pass
+
+    def log_out(self):      # Log out qismi -> Name
+        pass
+
+    def update_login(self):  # Loginni yangilash qismi -> Name
+        pass
+
+    def update_password(self):  # Parolni yangilash qismi -> Name
+        pass
+
+
+account = Authorization()
